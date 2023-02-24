b45a4914f348bbd04405534ff503d7cc39c7e019
Gusti Nur Islam <gustinurislam@gmail.com>
2023-02-24 08:04:25 +0700

    updated

--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -6,14 +6,21 @@ import SelectField from "../components/SelectField"
 import TextFields from "../components/TextFields"
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
+import { pawdRegExp, phoneRegExp } from "../utils"
 
 // create schema validation
 const schema = yup.object({
-    fullName: yup.string().required('Full Name is required')
+    fullName: yup.string().required('Full Name is required'),
+    email: yup.string().required('Email is required').email(),
+    mobile: yup.string().required('Mobile Phone is required').matches(phoneRegExp, 'Phone number is not valid'),
+    country: yup.string().required('Country is required'),
+    password: yup.string().required('Password is required').matches(pawdRegExp, 'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character'),
+    confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Password must match'),
+    privacy: yup.bool().oneOf([true], 'Field must be checked'),
 })
 
 const RegisterForm = () => {
-    const { handleSubmit, formState: { errors }, control } = useForm({
+    const { handleSubmit, reset, formState: { errors }, control } = useForm({
         defaultValues: {
             fullName: '',
             email: '',
@@ -28,6 +35,7 @@ const RegisterForm = () => {
 
     const onSubmit = (data) => {
         console.log(data);
+        reset();
     }
 
     return (
@@ -37,7 +45,7 @@ const RegisterForm = () => {
             mt: '4rem',
             alignItems: 'center'
         }}>
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
                 <HowToReg />
             </Avatar>
             <Typography component='h1'>
